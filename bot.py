6286ab8366b95fc74114357afe9d1e00981b2c31
Suramusic <123370264+Suramusic@users.noreply.github.com>
2023-02-02 12:26:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "5713245787:AAF-x7x7vbxcWO98HFbrkPI8FDIH24iw4Ww")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "24531402"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "0f75926e95a476ccff47dacb52079053")
 
 STRING = os.environ.get("STRING", "")
 
